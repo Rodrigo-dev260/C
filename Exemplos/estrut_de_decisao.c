6bd5793e2da7c65b17a72374609469290a5ae388
Rodrigo-dev260 <rodrigo_stringari@hotmail.com>
2025-12-01 16:11:01 -0300

    trreinando

---
+++ Exemplos/estrut_de_decisao.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int a, b, soma;
+
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &a);
+
+    printf("Digite outro numero inteiro: ");
+    scanf("%d", &b);
+
+    soma = a + b;
+    printf("O valor da soma = %d \n", soma);
+
+    if(soma < 10)
+        printf("O valor da soma e maior que 10 \n");
+        printf("Valor alto\n");
+
+    return(0);
+}
